5fe444d357b717c3725817aa3f5fdd30fb207f2e
mdidato <f>
2015-07-19 18:31:36 +0100

    adds get geolocation

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -2,4 +2,9 @@ class GeolocationsController < ApplicationController
   def create
     Geolocation.create!(watchbug_id:params[:id],lat:params[:lat],long:params[:long],alt: params[:alt])
   end
+
+  def show
+    @geolocation = Geolocation.find_by(watchbug_id:params[:id])
+    render json: @geolocation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
     post '/geolocations/:id', to: 'geolocations#create', as:'geolocations_create'
+    get '/geolocations/:id', to: 'geolocations#show', as:'geolocations_show'
 end
