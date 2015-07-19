a6aafebfa2b7c81f213903d1398c6cfe8b7a0d55
mdidato <f>
2015-07-19 17:41:20 +0100

    removes profiling

--- config/boot.rb
+++ config/boot.rb
@@ -1,18 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'benchmark'
-
-def require(file)
-  @@first ||= Time.now
-  rc = false
-  ts = Benchmark.measure do
-    rc = super
-  end
-  if ENV['DEBUG_REQUIRE'].to_f < ts.total
-    total = ts.format("%t require #{file}")
-    from_start = (Time.now - @@first).to_i
-    $stdout.puts "#{total} (#{from_start} second(s) from start)"
-  end
-  rc
-end
