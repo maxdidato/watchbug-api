a476a1bc1f5e2a891b1659dc59e5fa8d6554fc7a
mdidato <f>
2015-07-19 17:04:14 +0100

    changes dev settings for db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150719141247) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "geolocations", force: :cascade do |t|
     t.string   "watchbug_id"
     t.string   "lat"
